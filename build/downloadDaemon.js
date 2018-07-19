25192447a3ece82add8db6326650f2c6d6db89e9
Thomas Zarebczan <thomas.zarebczan@gmail.com>
2018-07-18 23:26:38 -0400

    Fix paths + del files

--- build/downloadDaemon.js
+++ build/downloadDaemon.js
@@ -11,7 +11,7 @@ const downloadDaemon = targetPlatform =>
   new Promise((resolve, reject) => {
     const daemonURLTemplate = packageJSON.lbrySettings.lbrynetDaemonUrlTemplate;
     const daemonVersion = packageJSON.lbrySettings.lbrynetDaemonVersion;
-    const daemonDir = path.join(__dirname,'../\\',packageJSON.lbrySettings.lbrynetDaemonDir);
+    const daemonDir = path.join(__dirname,'../',packageJSON.lbrySettings.lbrynetDaemonDir);
     var daemonFileName = packageJSON.lbrySettings.lbrynetDaemonFileName;
 
     let currentPlatform = os.platform();
@@ -21,10 +21,10 @@ const downloadDaemon = targetPlatform =>
      daemonFileName = daemonFileName + '.exe';
     }
 
-    const daemonFilePath = path.join(daemonDir,'\\',daemonFileName);
-    const daemonVersionPath = path.join(__dirname, '\\daemon.ver');
+    const daemonFilePath = path.join(daemonDir,daemonFileName);
+    const daemonVersionPath = path.join(__dirname, 'daemon.ver');
     const daemonPlatform = targetPlatform || currentPlatform;
-    const tmpZipPath = path.join(__dirname, '\\..\\dist\\daemon.zip');
+    const tmpZipPath = path.join(__dirname, '../','dist\\daemon.zip');
     const daemonURL = daemonURLTemplate
       .replace(/DAEMONVER/g, daemonVersion)
       .replace(/OSNAME/g, daemonPlatform);
@@ -63,11 +63,12 @@ const downloadDaemon = targetPlatform =>
               });
             })
         )
-        .then(() => del(`${daemonVersionPath}*`))
+        .then(() => del(`${daemonFilePath}*`))
         .then(() => decompress(tmpZipPath, daemonDir, {
           filter: file =>
             path.basename(file.path) === daemonFileName,
         }))
+        .then(() => del(`${tmpZipPath}*`))
         .then(() => {
           console.log('\x1b[32msuccess\x1b[0m Daemon downloaded!');
           if (hasDaemonVersion) {
